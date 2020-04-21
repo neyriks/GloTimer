6c6c9e497ac8937002f7f5aff86d39846b6cb8b7
neyriks <mesuppgg@gmail.com>
2020-04-22 02:31:15 +0300

    18 урок

---
+++ days/main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const data = new Date();
+const weekday = new Array(7);
+weekday[0] = "Воскресенье";
+weekday[1] = "Понедельник";
+weekday[2] = "Вторник";
+weekday[3] = "Среда";
+weekday[4] = "Четверг";
+weekday[5] = "Пятница";
+weekday[6] = "Суббота";
+const helloDay = new Date().getHours();
+function helloBuddy() {
+    if (helloDay > 1 && helloDay < 4) {
+        console.log('Доброй ночи');
+    } else if (helloDay > 5 && helloDay < 12) {
+        console.log('Доброе утро');
+    } else if (helloDay > 13 && helloDay < 23) {
+        console.log('Добрый день');
+    } else if (helloDay > 24 && helloDay < 1) {
+        console.log('Доброй ночи');
+    }
+}
+helloBuddy();
+console.log('Сегодня: ' + weekday[data.getDay()]);
+console.log('Текущее время: ' + data.toLocaleTimeString('en'));
+function countTimer(deadline) {
+    function getTimeRemaining() {
+        const dateStop = new Date(deadline).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateStop - dateNow) / 1000,
+            seconds = Math.floor(timeRemaining % 60),
+            minutes = Math.floor((timeRemaining / 60) % 60),
+            hours = Math.floor(timeRemaining / 60 / 60) % 24,
+            days = Math.floor((timeRemaining / 60 / 60) / 24);
+        console.log('До нового года осталось: ' + days);
+        return { timeRemaining, hours, minutes, seconds, days };
+    }
+    getTimeRemaining();
+}
+countTimer('1 january 2021');
+
+
--- script/script.js
+++ script/script.js
@@ -1,1 +1,45 @@
-'use strict'
+'use strict';
+window.addEventListener('DOMContentLoaded', () => {
+
+
+    // Timer
+    function countTimer(deadline) {
+        const timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
+
+
+        function getTimeRemaining() {
+            const dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
+            return { timeRemaining, hours, minutes, seconds };
+        }
+
+        function formatTime(time) {
+            if (time < 10) {
+                time = '0' + time;
+            }
+            return time;
+        }
+
+
+        setInterval(() => {
+            const timer = getTimeRemaining();
+            timerHours.textContent = formatTime(timer.hours);
+            timerMinutes.textContent = formatTime(timer.minutes);
+            timerSeconds.textContent = formatTime(timer.seconds);
+        }, 1000);
+        if (getTimeRemaining <= 0) {
+            clearInterval(countTimer);
+            timerHours.textContent = '00';
+            timerMinutes.textContent = '00';
+            timerSeconds.textContent = '00';
+        }
+
+    }
+    countTimer('23 april 2020');
+});
