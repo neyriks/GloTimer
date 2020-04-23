38ed91d56236c6f51085aabfc5ec36ad38bf2a21
neyriks <mesuppgg@gmail.com>
2020-04-23 21:16:07 +0300

    Задание 19

--- script/script.js
+++ script/script.js
@@ -53,9 +53,21 @@ window.addEventListener('DOMContentLoaded', () => {
         btnMenu.addEventListener('click', handlerMenu);
         closeBtn.addEventListener('click', handlerMenu);
 
-        menuItems.forEach(elem => elem.addEventListener('click', handlerMenu, () => {
-            menuItems.scrollIntoView({ behavior: 'smooth' });
-        }));
+        menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+        const anchors = document.querySelectorAll('li>a[href^="#"]'),
+            btnScrollDown = document.querySelector('a[href="#service-block"]'),
+            links = [...anchors, btnScrollDown];
+
+        for (const anchor of links) {
+            anchor.addEventListener('click', event => {
+                event.preventDefault();
+                const blockID = anchor.getAttribute('href');
+                document.querySelector('' + blockID).scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            });
+        }
         // Убираем анимацию у телефонов
         let count = -100;
         const menuAnimation = () => {
@@ -77,14 +89,42 @@ window.addEventListener('DOMContentLoaded', () => {
     const togglePopUp = () => {
         const popup = document.querySelector('.popup'),
             popupBtn = document.querySelectorAll('.popup-btn'),
-            popupClose = document.querySelector('.popup-close');
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content');
         popupBtn.forEach(elem => {
             elem.addEventListener('click', () => {
                 popup.style.display = 'block';
+                popupContent.style.opacity = 0;
+                function animate({ duration, draw, timing }) {
+                    const start = performance.now();
+                    requestAnimationFrame(function animate(time) {
+                        let timeFraction = (time - start) / duration;
+                        if (timeFraction > 1) timeFraction = 1;
+                        const progress = timing(timeFraction);
+                        draw(progress);
+                        if (timeFraction < 1) {
+                            requestAnimationFrame(animate);
+                        }
+                    });
+                }
+                animate({
+                    duration: 1000,
+                    timing(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw(progress) {
+                        popupContent.style.opacity = progress * 1;
+                    },
+                });
             });
         });
         popupClose.addEventListener('click', () => {
             popup.style.display = 'none';
+        });
+        window.addEventListener('click', e => {
+            if (e.target === popup) {
+                popup.style.display = 'none';
+            }
         });
     };
     togglePopUp();
